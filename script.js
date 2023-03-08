18efd91bd0b4116cb6f2b54742742cd8f466994d
Alexandre <Alexandresossou@gmail.com>
2023-03-08 15:08:57 +0100

    frontend last

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-// Get references to the form and input fields
-const form = document.querySelector('.container form');
+// Get the HTML elements
 const departureInput = document.getElementById('departure');
 const arrivalInput = document.getElementById('arrival');
 const dateInput = document.getElementById('date');
+const searchButton = document.querySelector('.btn');
+const boxRight = document.querySelector('.box-right');
 
-// Add an event listener for the form submission
-form.addEventListener('submit', function(event) {
-  // Prevent the default form submission behavior
-  event.preventDefault();
+// Add an event listener to the "Search" button
+searchButton.addEventListener('click', () => {
+  // Get the values of the input fields
+  const departure = departureInput.value;
+  const arrival = arrivalInput.value;
+  const date = dateInput.value;
 
-  // Retrieve the values of the input fields
-  const departureValue = departureInput.value;
-  const arrivalValue = arrivalInput.value;
-  const dateValue = dateInput.value;
+  // Check if any input field is empty
+  if (!departure || !arrival || !date) {
+    boxRight.innerHTML = `<p>Please fill in all the fields.</p>`;
+    return;
+  }
 
-  // Do something with the form data
-  console.log(`Departure: ${departureValue}, Arrival: ${arrivalValue}, Date: ${dateValue}`);
+  // Create a message to display in the box-right div
+  const message = `Your journey from ${departure} to ${arrival} on ${date} is being processed...`;
+
+  // Update the box-right div with the message
+  boxRight.innerHTML = `<p>${message}</p>`;
+
+  // Simulate a search delay of 2 seconds
+  setTimeout(() => {
+    // Check if any result is found
+    const resultFound = false; // replace this with your own code to check if a result is found
+
+    if (resultFound) {
+      // Display the result
+      // Replace this with your own code to display the result
+    } else {
+      // Display the "not found" message
+      boxRight.innerHTML = `<img src="./images/notfound.png" alt="not found image"><p>Trip not found</p>`;
+    }
+  }, 2000); // change the delay time as needed
 });
+
